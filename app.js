312daefc9862dbfe8d8a974485b8561b4e325639
arrivance <arrivance@gmail.com>
2017-10-08 20:26:26 +0100

    fix minor regression whereby the last sentence of the bind would not be outputted.

--- app.js
+++ app.js
@@ -6,38 +6,46 @@ function copypasta() {
     currentAlias = 0
     // remove speech marks, and split the textarea into words
     var input = document.getElementById("input").value.replace("\"", "'").trim().replace(/(\r\n|\n|\r)/gm,"").split(" ");
-    document.getElementById("output").value = `alias "${bindName}" "${bindName}0"` + "\n"
-    for (var i = 0; i < input.length ;i++) {
-        let currentWord = input[i]
-        if (sentenceLine.length + currentWord.length > sourceWordLimit) {
-            if (i == input.length) { 
-                document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
-            }
-            else { 
-                document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}${currentAlias+1}"` + "\n"
-                currentAlias += 1
-                sentenceLine = currentWord + " "
-            }
-        }
-        else if (currentWord.slice(-1) == ".") {
-            if (i+1 == input.length) { 
-                sentenceLine += currentWord + " "
-                document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
-            }
-            else { 
-                sentenceLine += currentWord + " "
-                document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}${currentAlias+1}"` + "\n"
-                currentAlias += 1
-                sentenceLine = ""
-            }
-        }
 
-        else if (i == input.length) {
-            sentenceLine += currentWord + " "
-            document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
-        }
-        else { 
-            sentenceLine += currentWord + " "
+    if (bindName == "") {
+        document.getElementById("output").value = "No bind name was given."
+    }
+
+    else { 
+        document.getElementById("output").value = `alias "${bindName}" "${bindName}0"` + "\n"
+        for (var i = 0; i < input.length ;i++) {
+            let currentWord = input[i]
+            // console.log(`debug output: currentWord: ${currentWord}, i: ${i}, sentenceLine: ${sentenceLine}, length of string: ${inputLen}`)
+            if (sentenceLine.length + currentWord.length > sourceWordLimit) {
+                if (i+1 == input.length) { 
+                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
+                }
+                else { 
+                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}${currentAlias+1}"` + "\n"
+                    currentAlias += 1
+                    sentenceLine = currentWord + " "
+                }
+            }
+            else if (currentWord.slice(-1) == ".") {
+                if (i+1 == input.length) { 
+                    sentenceLine += currentWord + " "
+                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
+                }
+                else { 
+                    sentenceLine += currentWord + " "
+                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}${currentAlias+1}"` + "\n"
+                    currentAlias += 1
+                    sentenceLine = ""
+                }
+            }
+
+            else if (i+1 == input.length) {
+                sentenceLine += currentWord + " "
+                document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
+            }
+            else { 
+                sentenceLine += currentWord + " "
+            }
         }
     }
 

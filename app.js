41c29c0ad5b7574356a868c545faa603cf628c1e
arrivance <arrivance@gmail.com>
2017-10-09 21:04:03 +0100

    v2: change javascript to follow jshint standards.

--- app.js
+++ app.js
@@ -1,54 +1,53 @@
-
-function copypasta() { 
-    sentenceLine = ""
-    sourceWordLimit = 126
-    bindName = document.getElementById("name").value
-    currentAlias = 0
+/*jshint esversion: 6 */
+function copypasta() {
+    "use strict";
+    var sentenceLine = "";
+    var sourceWordLimit = 126;
+    var bindName = document.getElementById("name").value;
+    var currentAlias = 0;
     // remove speech marks, and split the textarea into words
     var input = document.getElementById("input").value.replace("\"", "'").trim().replace(/(\r\n|\n|\r)/gm,"").split(" ");
 
     if (bindName == "") {
-        document.getElementById("output").value = "No bind name was given."
+        document.getElementById("output").value = "No bind name was given.";
     }
 
-    else { 
-        document.getElementById("output").value = `alias "${bindName}" "${bindName}0"` + "\n"
-        for (var i = 0; i < input.length ;i++) {
-            let currentWord = input[i]
+    else {
+        document.getElementById("output").value = `alias "${bindName}" "${bindName}0"` + "\n";
+        for (var i = 0; i < input.length; i++) {
+            let currentWord = input[i];
             // console.log(`debug output: currentWord: ${currentWord}, i: ${i}, sentenceLine: ${sentenceLine}, length of string: ${inputLen}`)
             if (sentenceLine.length + currentWord.length > sourceWordLimit) {
-                if (i+1 == input.length) { 
-                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
+                if (i+1 == input.length) {
+                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n";
                 }
-                else { 
-                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}${currentAlias+1}"` + "\n"
-                    currentAlias += 1
-                    sentenceLine = currentWord + " "
+                else {
+                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}${currentAlias+1}"` + "\n";
+                    currentAlias += 1;
+                    sentenceLine = currentWord + " ";
                 }
             }
             else if (currentWord.slice(-1) == ".") {
-                if (i+1 == input.length) { 
-                    sentenceLine += currentWord + " "
-                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
+                if (i+1 == input.length) {
+                    sentenceLine += currentWord + " ";
+                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n";
                 }
-                else { 
-                    sentenceLine += currentWord + " "
-                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}${currentAlias+1}"` + "\n"
-                    currentAlias += 1
-                    sentenceLine = ""
+                else {
+                    sentenceLine += currentWord + " ";
+                    document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}${currentAlias+1}"` + "\n";
+                    currentAlias += 1;
+                    sentenceLine = "";
                 }
             }
 
             else if (i+1 == input.length) {
-                sentenceLine += currentWord + " "
-                document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n"
+                sentenceLine += currentWord + " ";
+                document.getElementById("output").value += `alias "${bindName}${currentAlias}" "${sentenceLine};alias ${bindName} ${bindName}0"` + "\n";
             }
-            else { 
-                sentenceLine += currentWord + " "
+            else {
+                sentenceLine += currentWord + " ";
             }
         }
     }
-
-
 }
 
